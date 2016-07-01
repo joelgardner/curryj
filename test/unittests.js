98fa36e163ad4d784f11f0c8731f57091bcb2e90
Joel Gardner <william.joel.gardner@gmail.com>
2016-06-30 21:01:09 -0600

    not sure how this was duped 3 times...

--- test/unittests.js
+++ test/unittests.js
@@ -31,35 +31,6 @@ describe('curryj library', function () {
     assert.equal(threeDx1y2z3.x, 'x1');
     assert.equal(threeDx1y2z3.y, 'y2');
     assert.equal(threeDx1y2z3.z, 'z3');
-  });
-
-  it('should return functions until all arguments are supplied', function () {
-    var threeD = curryj.curry(_3dcoords),
-        threeDx1 = threeD('x1'),
-        threeDx1y2 = threeDx1('y2'),
-        threeDx1y2z3 = threeDx1y2('z3');
-    assert.equal(typeof(threeD), 'function');
-    assert.equal(typeof(threeDx1), 'function');
-    assert.equal(typeof(threeDx1y2), 'function');
-    assert.equal(typeof(threeDx1y2z3), 'object');
-    assert.equal(threeDx1y2z3.x, 'x1');
-    assert.equal(threeDx1y2z3.y, 'y2');
-    assert.equal(threeDx1y2z3.z, 'z3');
-    assert.equal(threeDx1y2(threeDx1y2z3).z.z, 'z3');
-  });
-
-  it('should return functions until all arguments are supplied', function () {
-    var threeD = curryj.curry(_3dcoords),
-        threeDx1 = threeD('x1'),
-        threeDx1y2 = threeDx1('y2'),
-        threeDx1y2z3 = threeDx1y2('z3');
-    assert.equal(typeof(threeD), 'function');
-    assert.equal(typeof(threeDx1), 'function');
-    assert.equal(typeof(threeDx1y2), 'function');
-    assert.equal(typeof(threeDx1y2z3), 'object');
-    assert.equal(threeDx1y2z3.x, 'x1');
-    assert.equal(threeDx1y2z3.y, 'y2');
-    assert.equal(threeDx1y2z3.z, 'z3');
     assert.equal(threeDx1y2(threeDx1y2z3).z.z, 'z3');
   });
 
